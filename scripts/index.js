ed03d01995629c1c4f97b5858fee90970adb586b
vixson <vixsonvx@gmail.com>
2020-08-29 20:54:54 +0100

    Added Payvest Project.
    :heavy_plus_sign: Manually switching dark mode on vx-logo click

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
+function switchMode() {
+  document.querySelector('body').classList.toggle('night');
+}
 (() => {
   const currentTime = new Date().getHours();
   if (currentTime < 6 || currentTime > 21) document.querySelector('body').classList.toggle('night');
